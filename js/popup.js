0db98ccda1c1604137ba880c3cb551367a5ef2f7
yunjung76 <111342237+yunjung76@users.noreply.github.com>
2022-08-19 15:21:24 +0900

    Add files via upload

---
+++ js/popup.js
@@ -0,0 +1,49 @@
+$(function(){
+    $(".pbox01").click(function(){
+        $(".popup01").show();
+    });
+
+      $(".exit").click(function(){
+        $(".popup01").hide();
+    });
+});
+
+$(function(){
+    $(".pbox02").click(function(){
+        $(".popup02").show();
+    });
+
+      $(".exit").click(function(){
+        $(".popup02").hide();
+    });
+});
+
+$(function(){
+    $(".pbox03").click(function(){
+        $(".popup03").show();
+    });
+
+      $(".exit").click(function(){
+        $(".popup03").hide();
+    });
+});
+
+$(function(){
+    $(".pbox04").click(function(){
+        $(".popup04").show();
+    });
+
+      $(".exit").click(function(){
+        $(".popup04").hide();
+    });
+});
+
+$(function(){
+    $(".pbox05").click(function(){
+        $(".popup05").show();
+    });
+
+      $(".exit").click(function(){
+        $(".popup05").hide();
+    });
+});
